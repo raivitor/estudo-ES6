024094a81316650dc51c915fcf1cccf161058af5
Raí Vitor <raivitor@gmail.com>
2017-02-18 17:58:57 -0300

    Melhorando a regex

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -10,10 +10,9 @@ class DateHelper {
     }
     
     static textoParaData(texto) {
-        
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) 
             throw new Error('Deve estar no formato aaaa-mm-dd');
-             
+                    // spread operator
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
